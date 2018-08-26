93c2e55f94dc964c9292707187f01e62a515dfa7
prasanthtootcat <prasanthtootcat@live.com>
2018-08-26 14:45:48 +0530

    Heroku build

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 var http = require('http');
 var url = require('url');
+var fs = require('fs');
 
 http.createServer(function(request, response) {
-	var queryData = url.parse(request.url, true).query;
-	const spawn = require("child_process").spawn;
-	const pythonProcess = spawn('python',["./webVersionDD.py",queryData.text]);
-	pythonProcess.stdout.on('data', (data) => {
-        response.writeHeader(200);
-        response.write(data);
-        response.end();
-	});
+	var q = url.parse(request.url, true);
+	if(!q.query.text){
+		fs.readFile('./index.html', function(err, data) {
+			if (err) throw err;
+    		response.writeHead(200, {'Content-Type': 'text/html'});
+    		response.write(data);
+    		response.end();
+  		});
+	}
+	else{
+		const spawn = require("child_process").spawn;
+		const pythonProcess = spawn('python',["./webVersionDD.py",q.query.text]);
+		pythonProcess.stdout.on('data', (data) => {
+        	response.writeHeader(200);
+        	response.write(data);
+        	response.end();
+		});	
+	}
+	
 }).listen(8080);
